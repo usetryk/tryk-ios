928bf4b9f0ef60926ab80145cc2da9b6d5e3e852
Ali Furkan Demirbaş <furkan.demirbas@usemagnetiq.com>
2022-01-01 17:00:58 +0300

    tryk ios app init

---
+++ tryk-ios/tryk-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tryk-ios
+//
+//  Created by Ali Furkan Demirbas on 1.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tryk-ios/tryk-ios/tryk_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryk_iosApp.swift
+//  tryk-ios
+//
+//  Created by Ali Furkan Demirbas on 1.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct tryk_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
